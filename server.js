2fd15dd52e63bfd3b5f7a577edeaeace3605a271
Rachel Harris <rjhart14@gmail.com>
2019-04-15 05:38:08 -0700

    test push

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ var PORT = process.env.PORT || 3000;
 var cheerio = require("cheerio");
 var axios = require("axios");
 
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+mongoose.connect(MONGODB_URI);
+
 // Require all models
 var db = require("./models");
 
@@ -25,23 +29,44 @@ app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Make public a static folder
-app.use(express.static("public"));
+app.use(express.static("public/"));
 
 // Connect to the Mongo DB
 mongoose.connect("mongodb://localhost/unit18Populater", { useNewUrlParser: true });
 
+//axios
+
+app.get("/", function(req, res) {
+    axios.get("https://www.pcgamer.com/news/").then(function(response) {
+        var $ = cheerio.load(response.data);
+        var results = [];
+        $(".listingResult").each(function(i, element) {
+            var title = $(element).find(".article-name").text();
+            var link = $(element).find("a").attr("href");
+
+            results.push({
+                title: title,
+                link: link
+            });
+        });
+        console.log(results);
+        res.render("index");
+    });
+});
+
+
 // Handlebars
 app.engine(
-  "handlebars",
-  exphbs({
-    defaultLayout: "main"
-  })
+    "handlebars",
+    handlebars({
+        defaultLayout: "main"
+    })
 );
 app.set("view engine", "handlebars");
 
 // Routes
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
+// require("./routes/apiRoutes")(app);
+// require("./routes/htmlRoutes")(app);
 
 // Start the server
 app.listen(PORT, function() {
